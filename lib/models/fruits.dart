c36f0d0ea480c0446f210f636eb822f4d8e3cd57
RakeshSeervi <rabs18is@cmrit.ac.in>
2020-11-16 02:26:30 +0530

    added list of fruits and vegetables

---
+++ lib/models/fruits.dart
@@ -0,0 +1,136 @@
+const String NAME = 'name';
+const String WEIGHTED = 'weighted';
+const String WEIGHTS = 'weights';
+const String MIN_WEIGHT = 'min-weight';
+const String MAX_WEIGHT = 'max-weight';
+const String IMAGE_PATH = 'assets/images/fruits/';
+const List WEIGHT1 = [0.5, 1, 5];
+const List WEIGHT2 = [0.25, 0.5, 1];
+
+const List Fruits = [
+  {
+    NAME: 'Apple',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Mango',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Banana',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Raw Banana',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Papaya',
+    WEIGHTED: false,
+    MIN_WEIGHT: 0.7,
+    MAX_WEIGHT: 1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Brown Coconut',
+    WEIGHTED: false,
+    MIN_WEIGHT: 0.4,
+    MAX_WEIGHT: 0.8,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Sweet Lime [Mosambi]',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Pomegranate',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Watermelon',
+    WEIGHTED: false,
+    MIN_WEIGHT: 1.25,
+    MAX_WEIGHT: 2.25,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Kiwi',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Pineapple',
+    WEIGHTED: false,
+    MIN_WEIGHT: 0.8,
+    MAX_WEIGHT: 1.2,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Avocado',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Muskmelon',
+    WEIGHTED: false,
+    MIN_WEIGHT: 0.6,
+    MAX_WEIGHT: 1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Custard Apple',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Gooseberry [Amla]',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT2,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Plums',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT2,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Pear',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Sapota [Chickoo]',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Orange',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+  {
+    NAME: 'Gauva',
+    WEIGHTED: true,
+    WEIGHTS: WEIGHT1,
+    IMAGE_PATH: IMAGE_PATH + '',
+  },
+];
